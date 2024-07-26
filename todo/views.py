7c4631dd591282817804db9221ad812efc665923
Taher Mahmood <taher.ccg@gmail.com>
2024-07-26 08:10:07 +0000

    Add:Create todo list html

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
-def say_hello(request):
-    return HttpResponse("Hello!, This is my 1st Django App!!")
+def get_todo_list(request):
+    return render(request, 'todo/todo_list.html')
+
+
 
